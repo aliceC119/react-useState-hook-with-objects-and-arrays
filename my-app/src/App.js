49adf9bc06d0ecdce7ac6157063b851560dee866
Alice Chan <alicebay10@yahoo.co.uk>
2024-12-15 11:04:21 +0000

    added an add item function and its button & added a remove item function and its button.

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,11 +1,11 @@
 import css from './App.module.css';
-import Sidebar from "./components/Sidebar";
+import UseStateWithArrays from "./components/UseStateWithArrays";
 
 function App() {
   return (
     <div className={css.App}>
       {/* Add your components here */}
-      <Sidebar />
+      <UseStateWithArrays />
     </div>
   );
 }
