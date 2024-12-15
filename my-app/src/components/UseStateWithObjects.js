1147e6c0a003be118012127c7dfb066f61e8a2ee
Alice Chan <alicebay10@yahoo.co.uk>
2024-12-15 11:23:05 +0000

    added ...name, inside the onChange for both firstName & lastName.

--- my-app/src/components/UseStateWithObjects.js
+++ my-app/src/components/UseStateWithObjects.js
@@ -9,14 +9,14 @@ function UseStateWithObjects() {
                 <input
                     type="text"
                     value={name.firstName}
-                    onChange={e => setName({
+                    onChange={e => setName({...name,
                         firstName: e.target.value
                     })}
                 />
                 <input
                     type="text"
                     value={name.lastName}
-                    onChange={e => setName({
+                    onChange={e => setName({...name,
                         lastName: e.target.value
                     })}
                 />
